f04275153bcd9a0fdedefee1363db375690fbe1f
Killjoybr <112730142+Killjoybr@users.noreply.github.com>
2023-09-01 02:04:00 -0300

    adicionando rota delete

--- src/routers/routes.js
+++ src/routers/routes.js
@@ -8,6 +8,8 @@ routes.get('/jogadores', jogadores.listarJogadores);
 routes.get('/jogadores/:id', jogadores.obterJogador);
 routes.post('/jogadores', jogadores.cadastrarJogador);
 routes.put('/jogadores/:id', jogadores.atualizarJogador);
+routes.patch('/jogadores/:id/nick', jogadores.atualizarNick);
+routes.delete('/jogadores/:id', jogadores.excluirJogador)
 
 // Exportando rotas
 module.exports = routes;
